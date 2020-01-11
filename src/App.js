4cefe0d8a9787596515fd7e96b7414a3b59b8739
lquevedo <lquevedo47@gmail.com>
2020-01-10 22:03:12 -0600

    Set up project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Error from './components/Error';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/about' exact component={About} />
+          <Route path='/' component={Error} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return(
+        <div>
+            <h1>Sorry. We could not the find the page that you were looking for.</h1>
+        </div>
+    );
+};
+
+export default Error;
